3e67251ee779bdba00e7a2775289360fe213660b
Nicholas Schuler <nicholas.schuler@student.uts.edu.au>
2017-08-17 21:28:26 +1000

    Backend for Extremely Basic User Regsistration and login

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+// Connect to Database
+mongoose.connect(config.database, {useMongoClient: true});
+
+// On Connection
+mongoose.connection.on('connected', () => {
+	console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+	console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users'); // Contains our User Routes
+
+// PORT Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors()); // Alows us to make a request to our API with a different domain name
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users); // Use /users for all our user routes
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+// Start Server
+app.listen(port, () => {
+	console.log('Server started on port '+port);
+});
