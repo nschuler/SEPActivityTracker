19e66aeecb8f859491c049f953b27200aff60532
corey-stidston <Corey.Stidston@student.uts.edu.au>
2017-09-07 22:27:44 +1000

    Updated the model to match the new User table

--- models/user.js
+++ models/user.js
@@ -4,24 +4,28 @@ const dateformat = require('dateformat');
 var mysql_query = require('../connection');
 
 
-function User(name, email, username, password) {
-	this.name = name,
-	this.email = email,
+function User(username, password, first_name, last_name, email, mobile, dob) {
 	this.username = username,
-	this.password = password
+	this.password = password,
+	this.first_name = first_name,
+	this.last_name = last_name,
+	this.email = email,
+	this.mobile = mobile,
+	this.dob = dob
 }
 
 module.exports = User;
 
 module.exports.getUserById = function(id, callback) {
-	mysql_query('SELECT * FROM users WHERE id = ?', id, callback);
+	mysql_query('SELECT * FROM User WHERE id = ?', id, callback);
 }
 
 module.exports.getUserByUsername = function(username, callback) {
-	mysql_query('SELECT * FROM users WHERE username = ?', username, callback);
+	mysql_query('SELECT * FROM User WHERE username = ?', username, callback);
 }
 
 module.exports.addUser = function(newUser, callback) {
+
 	// Generate a salt for the password of 10 rounds
 	bcrypt.genSalt(10, (err, salt) => {
 		bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -32,16 +36,17 @@ module.exports.addUser = function(newUser, callback) {
 			date = dateformat(now, 'yyyy-mm-dd HH:MM:ss');
 
 			let user = {
-				name: newUser.name,
 				username: newUser.username,
 				password: newUser.password,
-				person_id: 12,
-				status: 'TEST',
+				first_name: newUser.first_name,
+				last_name: newUser.last_name,
+				email: newUser.email,
+				mobile: newUser.mobile,
+				dob: newUser.dob,
 				created_at: date,
 				last_updated_at: date,
-				email: newUser.email
 			};
-			mysql_query('INSERT INTO users SET ?', user, callback);
+			mysql_query('INSERT INTO User SET ?', user, callback);
 		});
 	}); 
 }
