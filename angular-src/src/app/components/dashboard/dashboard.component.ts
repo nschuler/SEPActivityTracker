ab5d9575adee64520b8b8c91d0a5efc0dec38327
Nicholas Schuler <nicholas.schuler@student.uts.edu.au>
2017-09-10 18:39:38 +1000

    Added Child selector for educators to add children to room. Only working with static children names. Needs backend complete to pull a complete list of children from the centres Database.

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Optional } from '@angular/core';
-import { MdDialog, MdDialogRef, MdSnackBar, MdTabsModule, MdButtonModule, MdGridListModule } from '@angular/material';
+import { MdDialog, MdDialogRef, MdSnackBar, MdTabsModule, MdButtonModule, MdGridListModule, MdAutocompleteModule } from '@angular/material';
+import {FormControl} from '@angular/forms';
 import { Http } from '@angular/http';
 import { FlashMessagesService } from 'angular2-flash-messages';
+
+
+import 'rxjs/add/operator/startWith';
 
 
 
@@ -11,15 +15,33 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
   spaceScreens: Array<any>;
   roomsArray = [];
 
 
+  childrenCtrl: FormControl;
+  filteredChildren: any;
+
+  allChildren = [
+    'Corey Stidston',
+    'Nicholas Schuler',
+    'Alan Bootheeeee',
+    'Chris Kerdad',
+    'Isaac the Rich Man',
+    'Tex the bad Handshaker'
+  ];
+
   constructor(
     private http: Http, 
     private flashMessage: FlashMessagesService
-    ){}
+    ){
+    this.childrenCtrl = new FormControl();
+    this.filteredChildren = this.childrenCtrl.valueChanges
+        .startWith(null)
+        .map(name => this.filterChildren(name));
+  }
 
 
   ngOnInit() {
@@ -50,5 +72,10 @@ export class DashboardComponent implements OnInit {
     console.log('event => ', $event.tab.textLabel);
     console.log('index => ', this.roomsArray[$event.index].content);
     this.roomsArray[$event.index].content = "YOLO : " + $event.index;
+  }
+
+  filterChildren(val: string) {
+    return val ? this.allChildren.filter(s => new RegExp(`^${val}`, 'gi').test(s))
+               : this.allChildren;
   }
 }
