e28e835b459768f09f6dff243526dab5d936d296
Alan Boothe <12021679@student.uts.edu.au>
2017-09-14 12:27:03 +1000

    Created schedule page for a single child with dynamically pulled information, basic structure for timetable page layout started but functionality for buttons is still needed.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule } from '@angular/material';
+import { MdDatepickerModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -18,19 +20,17 @@ import { TimetableComponent } from './components/timetable/timetable.component';
 
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
-import { ParentService } from './services/parent.service';
-import { EducatorService } from './services/educator.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
-
+import { ParentService } from './services/parent.service';
 
 
 const appRoutes: Routes = [
-	{path:'', component:HomeComponent},
-	{path:'register', component:RegisterComponent},
-	{path:'login', component:LoginComponent},
-	{path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard]},
-	{path:'profile', component:ProfileComponent, canActivate:[AuthGuard]},
+  {path:'', component:HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'profile', component:ProfileComponent, canActivate:[AuthGuard]},
   {path:'timetable', component:TimetableComponent}
 ]
 
@@ -52,9 +52,11 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdDatepickerModule
   ],
-  providers: [ValidateService, AuthService, AuthGuard, ParentService, EducatorService],
+  providers: [ValidateService, AuthService, AuthGuard, ParentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/timetable/timetable.component.ts
+++ angular-src/src/app/components/timetable/timetable.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDatepickerModule } from '@angular/material';
+import { MdGridListModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
 import { AuthService } from '../../services/auth.service';
 import { ParentService } from '../../services/parent.service';
-import { EducatorService } from '../../services/educator.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,32 +12,62 @@ import { Router } from '@angular/router';
   styleUrls: ['./timetable.component.css']
 })
 export class TimetableComponent implements OnInit {
+  firstName: string;
+  familyName: string;
+  dob: string;
+  address: string;
+  allergies: string;
+  famId: string;
+  childId: string;
+  roomId: string;
+  boolLike: boolean;
 
-  constructor(private authService: AuthService, private parentService: ParentService, private educatorService: EducatorService, private router: Router) { }
+  constructor(private authService: AuthService, private parentService: ParentService, private router: Router) {
+
+  }
 
   ngOnInit() {
-  	this.parentService.getTimetable().subscribe(data => {
-  		console.log(data.msg);
-  	},
-  	err => {
-  		console.log(err);
-  		return false;
-  	});
+    this.parentService.getTimetable().subscribe(data => {
+      console.log(data.msg);
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
 
     this.parentService.getFamily().subscribe(data => {
-      console.log(data);
+      console.log(data.family);
+      this.firstName = data.family.children[0].first_name;
+      this.familyName = data.family.familyName;
+      this.dob = data.family.children[0].dob;
+      this.address = data.family.address;
+      this.allergies = data.family.children[0].allergens;
+      this.famId = data.family.children[0].family_id;
+      this.childId = data.family.children[0].id;
+      this.roomId = data.family.children[0].room_id;
+      this.boolLike = false;
     },
     err => {
       console.log(err);
       return false;
     });
+  }
 
-    this.educatorService.getChildren().subscribe(data => {
-      console.log(data);
-    },
-    err => {
-      console.log(err);
-      return false;
-    });
+  openDatepicker() {
+    console.log("Clickity click click!");
+  }
+
+  likeActivity() {
+    if(this.boolLike == true) {
+      console.log("You have unliked this activity!");
+      this.boolLike = false;
+    } else {
+      console.log("You have liked this activity!");
+      this.boolLike = true;
+    }
+  }
+
+  leaveComment() {
+    console.log("Leave a comment!");
   }
 }
