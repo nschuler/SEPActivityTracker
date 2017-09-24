6cbffca5d81357e95123ab58cbbb0d4268eae6bc
Nicholas Schuler <nicholas.schuler@student.uts.edu.au>
2017-09-24 22:04:53 +1000

    Basic Room create and activity create forms. No functionality as of yet.

--- angular-src/src/app/components/activities/activities-create.component.ts
+++ angular-src/src/app/components/activities/activities-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MdButtonModule } from '@angular/material';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-activities-create',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities-create.component.css']
 })
 export class ActivitiesCreateComponent implements OnInit {
+  activityCreated:boolean = false;
 
-  constructor() { }
+  // Make this dynamically pulled from DB
+  activityTypes = [{id:1, name: "care"}, {id:2, name: "learning"}, {id:1, name: "leisure"}];
+
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
 
+  newActivity(activity_name:string, activity_description:string, activity_type:string){
+        console.log(activity_name);
+   }
+
+   goBack(): void{
+        this.location.back();
+   }
 }
--- angular-src/src/app/components/rooms/room-add.component.ts
+++ angular-src/src/app/components/rooms/room-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MdButtonModule } from '@angular/material';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-room-add',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-add.component.css']
 })
 export class RoomAddComponent implements OnInit {
+  roomCreated:boolean = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private location: Location) { }
 
   ngOnInit() {
   }
+
+  newRoom(room_name:string, room_description:string, birthDate:Date){
+        console.log(room_name);
+   }
+
+   goBack(): void{
+        this.location.back();
+   }
+
 
 }
