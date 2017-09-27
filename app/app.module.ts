05498ca7208a4cd09f817351c45d99de346ecb04
Alan Boothe <12021679@student.uts.edu.au>
2017-09-27 13:15:26 +1000

    Modified interface for timetable route, all data displayed is now pulled dynamically from DB.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdCardModule } from '@angular/material';
 import { MdDatepickerModule } from '@angular/material';
+import { DatePickerModule } from 'ng2-datepicker';
 import 'hammerjs';
-import { OrderModule } from 'ngx-order-pipe';
-import { DndModule } from 'ng2-dnd';
-
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -26,15 +24,14 @@ import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
 import { ParentService } from './services/parent.service';
-import { EducatorService } from './services/educator.service';
 
 
 const appRoutes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'register', component:RegisterComponent},
-  {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard]},
-  {path:'profile', component:ProfileComponent, canActivate:[AuthGuard]},
+	{path:'', component:HomeComponent},
+	{path:'register', component:RegisterComponent},
+	{path:'login', component:LoginComponent},
+	{path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard]},
+	{path:'profile', component:ProfileComponent, canActivate:[AuthGuard]},
   {path:'timetable', component:TimetableComponent}
 ]
 
@@ -52,7 +49,6 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule,
@@ -60,10 +56,9 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     MdCardModule,
     MdDatepickerModule,
-    OrderModule,
-    DndModule.forRoot()
+    DatePickerModule
   ],
-  providers: [ValidateService, AuthService, AuthGuard, ParentService, EducatorService, DndModule],
+  providers: [ValidateService, AuthService, AuthGuard, ParentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
